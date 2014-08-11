7bbd9e5597721e1ca8cd317c99459632bf630ee9
Cheuk Wing Leung <cheuk.wing.leung@external.atlascopco.com>
2014-08-11 16:48:13 +0200

    fixing in progress of sl

--- sl.py
+++ sl.py
@@ -1,3 +1,4 @@
+import requests
 import common
 import util
 import sys
@@ -6,20 +7,19 @@ import os
 class Wiki(object):
   """A class of Wiki enquiry
   """
-  WIKIAPI       = "http://api.sl.se/api2/realtimedepartures.xml"
+  
+  WIKIAPI       = "http://api.sl.se/api2/realtimedepartures.json"
   WIKIAPI_FLAGS = {
     "key"          : "86564990d30b4fc496406e1b93c12686",
     "timewindow"   : "30",
   }
 
-  def __init__(self, titles):
+  def __init__(self, siteid):
     """Constructor of the Wiki enquiry
     @param titles titles to be searched
     """
     ## titles to be searched
-    self.titles = titles
-    ## keywords
-    self.keywords = None
+    self.siteid = siteid
     ## XML raw results
     self.raw_results = None
     ## Printable results
@@ -36,33 +36,49 @@ class Wiki(object):
   def format_keywords(self):
     """Format the keywords
     """
-    formatted_titles = common.format_texts(self.titles);
-    self.keywords = "siteid=" + formatted_titles
+    self.keywords = "siteid=%d" % self.siteid
     return
 
   def get_content(self):
     """Get the content of the wiki enquiry
     """
     formatted_url = common.format_url(self.WIKIAPI, self.WIKIAPI_FLAGS, self.keywords);
-    self.raw_results = common.get_url_content(formatted_url)
+    self.raw_results = requests.get(formatted_url)
     return
     
   def format_results(self):
     """Extract the results from a wiki XML content
     """  
-    metro_results = util.get_contents(self.raw_results, 'Metros')
-    bus_results = util.get_contents(self.raw_results, 'Buses')
-    train_results = util.get_contents(self.raw_results, 'Trains')
-    tram_results = util.get_contents(self.raw_results, 'Trams')
-    self.results = self.form_results_text(metro_results, bus_results, train_results, tram_results)
+    metro_results = self.raw_results.json()["ResponseData"]["Metros"]
+    bus_results = self.raw_results.json()["ResponseData"]["Buses"]
+    train_results = self.raw_results.json()["ResponseData"]["Trains"]
+    tram_results = self.raw_results.json()["ResponseData"]["Trams"]
+    
+    all_traffic = []
+    for metro in metro_results:
+      all_traffic.append(metro)
+    for bus in bus_results:
+      all_traffic.append(bus)
+    for train in train_results:
+      all_traffic.append(train)
+    for tram in tram_results:
+      all_traffic.append(tram)
+    
+    self.results = self.form_results_text(all_traffic)
     return
   
-  def form_results_text(self, metros, buses, trains, tram):
-    return len(buses)
+  def form_results_text(self, all_traffic):
+    seq = [transport["ExpectedDateTime"] for transport in all_traffic]
+    
+    return min(seq)
+  
+  def format_a_traffic(self, traffic):
+    return 
     
 def main():
     sys.argv.pop(0)
-    wiki = Wiki(sys.argv)
+    #wiki = Wiki(sys.argv[1])
+    wiki = Wiki(4634)
     print wiki.get_results()
 
 if __name__ == "__main__":
