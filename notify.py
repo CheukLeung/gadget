8854944845b91b7b7070713c046d778f22796422
CheukLeung <cheuk@CheukLeungs-MacBook-Pro.local>
2014-08-23 21:24:19 +0200

    Added support to OS X notification

--- notify.py
+++ notify.py
@@ -1,8 +1,16 @@
-import dbus
-import sys
+import platform
 
 def notify(summary, body='', app_name='', app_icon='',
+           timeout=5000, actions=[], hints=[], replaces_id=0):
+  if platform.system() == 'Linux':
+    notify_linux(summary, body, app_name, app_icon, timeout, actions, hints, replaces_id)
+  elif platform.system() == 'Darwin':
+    notify_mac(summary, body, app_name, app_icon, timeout, actions, hints, replaces_id)
+
+def notify_linux(summary, body='', app_name='', app_icon='',
     timeout=5000, actions=[], hints=[], replaces_id=0):
+  import dbus
+  import sys
   _bus_name = 'org.freedesktop.Notifications'
   _object_path = '/org/freedesktop/Notifications'
   _interface_name = _bus_name
@@ -12,6 +20,22 @@ def notify(summary, body='', app_name='', app_icon='',
   interface = dbus.Interface(obj, _interface_name)
   interface.Notify(app_name, replaces_id, app_icon,
       summary, body, actions, hints, timeout)
+
+
+def notify_mac(summary, body='', app_name='', app_icon='',
+               timeout=5000, actions=[], hints=[], replaces_id=0):
+  from Foundation import NSUserNotification
+  from Foundation import NSUserNotificationCenter
+  from Foundation import NSUserNotificationDefaultSoundName
+  notification = NSUserNotification.alloc().init()
+  notification.setTitle_(summary)
+  notification.setInformativeText_(body)
+  notification.setSoundName_(NSUserNotificationDefaultSoundName)
+  #notification.setImage("svd.jpg")
+  center = NSUserNotificationCenter.defaultUserNotificationCenter()
+  center.deliverNotification_(notification)
+
+
 
 # If run as a script, just display the argv as summary
 if __name__ == '__main__':
